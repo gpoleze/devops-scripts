0e4180399cf4add90b8e22280bc69a17c9b6c7b3
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2025-06-02 21:14:03 -0700

    moved to gitlab.com/gabriel.poleze/my-commands

--- utils/aws-utils.go
+++ utils/aws-utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+import (
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/jedib0t/go-pretty/v6/table"
+)
 
 func FilterTagByKey(tags []types.Tag, keyName string) string {
 	value := ""
@@ -10,4 +13,15 @@ func FilterTagByKey(tags []types.Tag, keyName string) string {
 		}
 	}
 	return value
+}
+
+func PrintOutput[T any](output string, list []T, itemToTableRow func(T) table.Row) {
+	switch output {
+	case "json":
+		PrintJson(list)
+	case "table":
+		BuildTable(list, itemToTableRow)
+	default:
+		BuildTable(list, itemToTableRow)
+	}
 }
